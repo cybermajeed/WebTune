fe173fbfb17a33fedb4d5c624130c81fe253cba6
Cyber Majeed <73933962+cybermajeed@users.noreply.github.com>
2023-09-01 20:52:05 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta name="Developer" content="CyberMajeed" />
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="style.css">
+  <link rel="shortcut icon" href="./assets/Favicon.png" type="image/x-icon">
+  <title>WebTune</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
